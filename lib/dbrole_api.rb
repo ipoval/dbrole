193cd01c9a1f3cb5ebbb828fa609670d5891f284
ipoval <ipoval@ya.ru>
2016-08-31 14:17:52 -0700

    Create dbrole_api.rb

---
+++ lib/dbrole_api.rb
@@ -0,0 +1,26 @@
+class ActiveRecord::Base
+  class_attribute :force_dbrole, instance_accessor: false
+end
+
+class ActiveRecord::ConnectionAdapters::ConnectionHandler
+  alias_method :'original retrieve_connection', :retrieve_connection
+
+  def retrieve_connection(klass)
+    if klass.force_dbrole
+      return send(:'original retrieve_connection', klass.force_dbrole)
+    end
+    send(:'original retrieve_connection', klass)
+  end
+end
+
+# Example of usage:
+#
+def dbrole(klass, dbrole, &block)
+  fail ArgumentError, 'provide a block to swith connection there' unless block_given?
+  fail ArgumentError, 'bad DB role class' unless dbrole.respond_to?(:connection)
+
+  klass.force_dbrole = dbrole
+  yield
+ensure
+  klass.force_dbrole = nil
+end
