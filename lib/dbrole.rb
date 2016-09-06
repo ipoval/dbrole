914e70f2414b7456507b0228bd0090615a089ea3
Ivan Povalyukhin <ipovalyukhin@yammer-inc.com>
2016-09-06 15:27:52 -0700

    added Mutex#synchronize wrapper

--- lib/dbrole.rb
+++ lib/dbrole.rb
@@ -2,6 +2,12 @@
 
 module DbRole
   class Manager
+    attr_accessor :lock
+
+    def initialize
+      @lock = Mutex.new
+    end
+
     def patch!
       Thread.current[:dbrole] ||= {}
 
@@ -9,12 +15,15 @@ module DbRole
         alias_method :'original retrieve_connection', :retrieve_connection
 
         def retrieve_connection(klass)
-          if Thread.current[:dbrole].present? && \
-             switch_to = Thread.current[:dbrole][klass.to_s]
-            return send(:'original retrieve_connection', switch_to)
-          end
+          ::DbRoleManager.lock.synchronize do
 
-          send(:'original retrieve_connection', klass)
+            if Thread.current[:dbrole].present? && \
+               switch_to = Thread.current[:dbrole][klass.to_s]
+              return send(:'original retrieve_connection', switch_to)
+            end
+            send(:'original retrieve_connection', klass)
+
+          end
         end
       end
     end
